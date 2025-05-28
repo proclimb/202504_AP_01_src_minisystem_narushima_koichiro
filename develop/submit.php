df07de22138a27e06854f48d1f66a43a76f56b92
Koichiro Narushima <202504ap01_narushima@x-ide.sakura.ne.jp>
2025-05-28 15:32:03 +0900

    submit.php

---
+++ develop/submit.php
@@ -0,0 +1,51 @@
+<?php
+
+// dd.phpとuser.phpを読み込む
+require_once 'db.php';
+require_once 'user.php';
+
+//confirm.phpから送られてきたデータを$dataに格納
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = [
+        'name'      => $_POST['name'],
+        'kana'      => $_POST['kana'],
+        'tel'       => $_POST['tel'],
+        'email'     => $_POST['email'],
+        'birthday'  => $_POST['birthday'],
+        'gender'    => $_POST['gender'],
+    ];
+}
+
+//$podから作った新しいインスタンスを$userに代入
+$user = new User($pod);
+
+// SQLを実行
+$user->create($data);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>完了画面</title>
+    <link rel="styleseet" href="style_new.css" j>
+</head>
+
+<body>
+    <div>
+        <h1>mini System</h1>
+    </div>
+    <div>
+        <h2>完了画面</h2>
+    </div>
+    <div>
+        <div>
+            <h1>登録完了</h1>
+            <p>登録ありがとうございました。</p>
+            <a href="index.php"><button type="button">TOPに戻る</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
