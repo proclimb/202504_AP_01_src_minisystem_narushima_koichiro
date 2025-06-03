15342d5c6abb58cdcc79428a95f300c82619c703
Koichiro Narushima <202504ap01_narushima@x-ide.sakura.ne.jp>
2025-06-03 14:53:46 +0900

    delete.php修正

--- develop/delete.php
+++ develop/delete.php
@@ -4,7 +4,7 @@ require_once 'db.php';
 require_once 'user.php';
 
 $id = $_POST["id"];
-$user = new User($id);
+$user = new User($pdo);
 $user->delete($id);
 
 ?>
@@ -26,11 +26,15 @@ $user->delete($id);
         <h2>削除完了画面</h2>
     </div>
     <div>
-        <h1>削除完了</h1>
-        <p>
-            削除しました。<br>
-        </p>
-        <button type="button">TOPに戻る</button>
+        <div>
+            <h1>削除完了</h1>
+            <p>
+                削除しました。<br>
+            </p>
+            <a href="index.php">
+                <button type="button">TOPに戻る</button>
+            </a>
+        </div>
     </div>
 </body>
 
